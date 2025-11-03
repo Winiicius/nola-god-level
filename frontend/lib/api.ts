d6c77cdd01a240130f4e7bf5cf7eb1003d268208
Winiicius <winiciusalexandre@gmail.com>
2025-11-03 18:54:07 -0300

    refactor: Limpa arquivo

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -1,14 +1,9 @@
 import axios from "axios";
 
-/**
- * Detecta automaticamente se está em ambiente local ou produção.
- * Usa a variável NEXT_PUBLIC_API_URL definida no .env.local (Vercel).
- */
-
-const baseURL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000"; // fallback para ambiente local
+const baseURL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000";
 
 export const api = axios.create({
-  baseURL: `${baseURL}`, // prefixo do backend FastAPI
+  baseURL: `${baseURL}`,
   headers: {
     "Content-Type": "application/json",
   },

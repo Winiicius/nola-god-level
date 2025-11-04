c8fc0530d4075193700b9327c50af987083e1752
Winiicius <winiciusalexandre@gmail.com>
2025-11-03 23:17:44 -0300

    refact: Muda abordagem de conexão com o Redis

--- backend/app/core/cache_service.py
+++ backend/app/core/cache_service.py
@@ -1,22 +1,13 @@
-import redis
+from upstash_redis import Redis
+from app.core.config import settings
 import json
 import hashlib
-
-from urllib.parse    import urlparse
-from app.core.config import settings
 
 
 class CacheService:
     def __init__(self):
-        redis_url = settings.REDIS_URL
-        parsed = urlparse(redis_url)
-
-        self.redis_client = redis.Redis(
-            host=parsed.hostname,
-            port=parsed.port or 6379,
-            password=parsed.password,
-            decode_responses=True
-        )
+        # Configura cliente Upstash
+        self.redis_client = Redis.from_url(settings.REDIS_URL, token=settings.REDIS_TOKEN)
 
     def _get_key(self, data: dict):
         key_base = json.dumps(data, sort_keys=True)
@@ -29,12 +20,12 @@ class CacheService:
 
     def set(self, key_data: dict, data, expire=300):
         key = self._get_key(key_data)
-        self.redis_client.setex(key, expire, json.dumps(data))
+        self.redis_client.set(key, json.dumps(data), ex=expire)
 
     def test_redis_connection(self):
         try:
-            self.redis_client.ping()
+            self.redis_client.set("healthcheck", "ok", ex=10)
             return "connected"
-        except redis.ConnectionError:
+        except Exception as e:
+            print(f"Redis connection error: {e}")
             return "unavailable"
-        
